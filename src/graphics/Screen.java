fb9481b990d77af684092bfc621a0427276dd73b
davidot <david.t@glazenkamp.net>
2014-06-15 13:01:15 +0200

    Added screen class for main graphics

---
+++ src/graphics/Screen.java
@@ -0,0 +1,12 @@
+package graphics;
+
+/**
+ * Created by David on 14-6-2014.
+ */
+public class Screen extends Render{
+
+
+    public Screen(int width, int height) {
+        super(width, height);
+    }
+}
