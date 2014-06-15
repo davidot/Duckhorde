7adb2b41887b0f9f6ee904cb41eb8daa7fde8145
davidot <david.t@glazenkamp.net>
2014-06-15 22:37:44 +0200

    Fixed render added test

--- src/graphics/Render.java
+++ src/graphics/Render.java
@@ -9,21 +9,21 @@ public class Render {
     public int height;
     public int[] pixels;
 
-    public Render(int width,int height) {
+    public Render(int width, int height) {
         this.width = width;
         this.height = height;
         pixels = new int[width * height];
     }
 
-    public void draw(Render render,int xOff,int yOff) {
-        for(int x = 0;x < render.width;x++) {
+    public void draw(Render render, int xOff, int yOff) {
+        for (int x = 0; x < render.width; x++) {
             int xNew = x + xOff;
-            if(xNew < 0 || xNew >= width) {
+            if (xNew < 0 || xNew >= width) {
                 continue;
             }
-            for(int y = 0;y < render.height;y++) {
-                int yNew = x + yOff;
-                if(yNew < 0 || yNew >= height) {
+            for (int y = 0; y < render.height; y++) {
+                int yNew = y + yOff;
+                if (yNew < 0 || yNew >= height) {
                     continue;
                 }
                 pixels[xNew + yNew * width] = render.pixels[x + y * render.width];
--- src/graphics/Screen.java
+++ src/graphics/Screen.java
@@ -1,12 +1,32 @@
 package graphics;
 
+import java.util.Random;
+
 /**
  * Created by David on 14-6-2014.
  */
-public class Screen extends Render{
+public class Screen extends Render {
 
+    private Render test;
 
     public Screen(int width, int height) {
         super(width, height);
+        test = new Render(200,200);
+        Random rand = new Random();
+        for(int i=0;i < 200 * 200;i++) {
+            System.out.println("adding:" + i);
+            test.pixels[i] = rand.nextInt();
+        }
     }
+
+    public void render() {
+        for(int i = 0;i<pixels.length;i++) {
+            pixels[i] = 0;
+        }
+
+        draw(test,0,0);
+
+
+    }
+
 }
